9ee54c62c1fc8dfffcb787da487c98eeeb0173cf
unknown <AL1458@ALIPL0238.accionlabs.in>
2017-06-08 18:25:16 +0530

    ss_db_ods

---
+++ ods-archiving/hive_schema_extract.sh
@@ -0,0 +1,14 @@
+rm -f tableNames.txt
+rm -f HiveTableDDL.txt
+hive -e "use $1; show tables;" > $1_tableNames.txt
+wait
+echo "CREATE database $1;" >>$1_HiveTableDDL.txt
+echo "USE $1;" >> $1_HiveTableDDL.txt
+cat $1_tableNames.txt |while read LINE
+   do
+      hive -e "use $1;show create table $LINE" >>$1_HiveTableDDL.txt
+      echo  -e "\n" >> $1_HiveTableDDL.txt
+    done
+    #rm -f tableNames.txt
+echo "Table DDL generated"
+
